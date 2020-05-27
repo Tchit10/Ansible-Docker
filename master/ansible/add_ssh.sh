07eb0b7fe9d67a1756dc2ae0465461e6a6cc265e
Tchit10 <tchit10@outlook.com>
2020-05-27 17:18:02 +0200

    Add playbook for Apache installation.

--- master/ansible/add_ssh.sh
+++ master/ansible/add_ssh.sh
@@ -1,8 +1,18 @@
 #!/bin/bash
-mkdir ~/.ssh
+ssh-keygen
+
+echo "Please enter the root passord for hosts :"
+read -r USERPASS
+
 ssh-keyscan -H ansible-docker_db_1 >> ~/.ssh/known_hosts
+sshpass -p "$USERPASS" ssh-copy-id root@ansible-docker_db_1
 
 for i in 1 2 3
 do
 	ssh-keyscan -H ansible-docker_web_$i >> ~/.ssh/known_hosts
+done
+
+for i in 1 2 3
+do
+	sshpass -p "$USERPASS" ssh-copy-id root@ansible-docker_web_$i
 done
